015cbef6905b760a20ece03c203f1c2dc75a6869
skunklab <theskunklab@gmail.com>
2020-01-17 04:52:51 -0600

    chg setupenv to dotnet 3.1

--- kubernetes/setupenv.cmd
+++ kubernetes/setupenv.cmd
@@ -1,7 +1,7 @@
 powershell.exe Set-ExecutionPolicy  -ExecutionPolicy Unrestricted -Scope LocalMachine
 powershell.exe Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
-powershell.exe Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/66adfd75-9c1d-4e44-8d9c-cdc0cbc41104/5288b628601e30b0fa10d64fdaf64287/dotnet-sdk-3.0.101-win-x64.exe -OutFile .\dotnet-sdk-3.0.101-win-x64.exe
-start /wait dotnet-sdk-3.0.101-win-x64.exe /install /norestart /quiet
+powershell.exe Invoke-WebRequest -Uri https://download.visualstudio.microsoft.com/download/pr/854ca330-4414-4141-9be8-5da3c4be8d04/3792eafd60099b3050313f2edfd31805/dotnet-sdk-3.1.101-win-x64.exe -OutFile .\dotnet-sdk-3.1.101-win-x64.exe
+start /wait dotnet-sdk-3.1.101-win-x64.exe /install /norestart /quiet
 powershell.exe Install-PackageProvider -Name NuGet -Force
 powershell.exe Install-Module -Name PowerShellGet -Force
 powershell.exe Update-Module -Name PowerShellGet
